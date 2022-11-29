910635b761ca4a06749de061fabd5e409aaca157
Utkarsh Jain <35839529+utkarshjain1508@users.noreply.github.com>
2022-11-29 08:03:35 +0000

    setting up codespace

--- src/Conductor.Steps/FunctionStep.cs
+++ src/Conductor.Steps/FunctionStep.cs
@@ -9,36 +9,82 @@ using System.Threading.Tasks;
 using WorkflowCore.Interface;
 using WorkflowCore.Models;
 
+ 
+
 namespace Conductor.Steps
 {
     public class FunctionStep : StepBody
     {
         public FunctionStep() { }
 
+ 
+
         public string FunctionAppURL { get; set; }
         public string FunctionAppKey { get; set; }
 
+ 
+
         public string Message { get; set; }
 
-        public string Status;
+ 
 
-        public string ResourceId;
+        public string Status { get; set; }
+
+ 
+
+        public int StatusCode { get; set; }
+
+ 
+
+        public string OrderId { get; set; }
+
+ 
+
+        public string UserName { get; set; }
+
+ 
+
+        public string ResponseMessage { get; set; }
+
+ 
+
         public class AzureFunctionResponse
         {
-            [JsonProperty("resourceId")]
-            public string ResourceId { get; set; }
+            [JsonProperty("message")]
+            public string Message { get; set; }
 
-            [JsonProperty("data")]
-            public string Data { get; set; }
+ 
+
+            [JsonProperty("orderId")]
+            public string OrderId { get; set; }
+
+ 
 
             [JsonProperty("status")]
             public string Status { get; set; }
+
+ 
+
+            [JsonProperty("statusCode")]
+            public int StatusCode { get; set; }
+
+ 
+
+            [JsonProperty("userName")]
+            public string UserName { get; set; }
         }
         public override ExecutionResult Run(IStepExecutionContext context)
         {
             var functionAppURL = FunctionAppURL;
             var requestObject = new JsonObject();
             requestObject.Add("name", Message);
+
+ 
+
+            if (String.IsNullOrEmpty(OrderId))
+            {
+                requestObject.Add("orderId", OrderId);
+            }
             var content = new StringContent(requestObject.ToString(), encoding: System.Text.Encoding.UTF8, "application/json");
             using (HttpClient client = new HttpClient())
             {
@@ -50,7 +96,10 @@ namespace Conductor.Steps
                         var responseString = responseContent.ReadAsStringAsync().Result;
                         var functionResult = JsonConvert.DeserializeObject<AzureFunctionResponse>(responseString);
                         Status = functionResult.Status;
-                        ResourceId = functionResult.ResourceId;
+                        OrderId = functionResult.OrderId;
+                        StatusCode = functionResult.StatusCode;
+                        UserName = functionResult.UserName;
+                        ResponseMessage = functionResult.Message;
                     }
                 }
             }           
